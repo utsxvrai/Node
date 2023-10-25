f61c4bee240755311a1ae75895db5a06b2f01311
Utsav Rai <utsavrai2115@gmail.com>
2023-10-25 14:41:15 +0530

    configuration

--- Learning/index.js
+++ Learning/index.js
@@ -1,3 +1,4 @@
+const config = require('config');
 const Joi = require('joi')
 const logger = require('./logger')
 const express = require('express');
@@ -9,7 +10,17 @@ app.use(express.json());
 app.use(express.urlencoded( { extended: true } ));
 app.use(express.static('public'));
 app.use(helmet());
-app.use(morgan('tiny'));
+
+// Configuration
+console.log('Application Name: ' + config.get('name'));
+console.log('Mail Server: ' + config.get('mail.host'));
+console.log('Mail Password: ' + config.get('mail.password'));
+
+if(app.get('env') === 'development'){
+    app.use(morgan('tiny'));
+    console.log('Morgan enabled...');
+}
+
 
 
 
